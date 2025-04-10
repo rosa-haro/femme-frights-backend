5edaa394176405671760a5d1dd1505de18c1c4e8
Rosa <rosa.harofdez@gmail.com>
2025-04-10 15:32:07 +0200

    Deleting secret.js

--- secret.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto")
-
-const secret = "Full stack 16 Rosa REFRESH";
-
-const secret2 = "Actualizo para que sea más difícil REFRESH"
-
-const hash = crypto.createHmac("sha256", secret).update(secret2).digest("hex")
-
-console.log(hash)
