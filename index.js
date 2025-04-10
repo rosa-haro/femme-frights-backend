338a16d7dba4a2934681c520981d668978ce379d
Rosa <rosa.harofdez@gmail.com>
2025-04-10 17:26:17 +0200

    Merge: bugfix/deployment

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const moviesRouter = require("./src/routes/moviesRouter")
 const loginRouter = require("./src/routes/loginRouter")
 const usersRouter = require("./src/routes/usersRouter")
 const cors = require("cors")
+const path = require("path"); 
 
 require("dotenv").config();
 const connectToDatabase = require("./src/config/db")
 
 const app = express();
 
-app.use(cors())
+const corsOptions = {
+    origin: process.env.NODE_ENV === 'production' 
+      ? "https://femme-frights-demo.vercel.app"  
+      : "http://localhost:5173",  
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'auth-token'],
+  };
+
+app.use(cors(corsOptions))
 connectToDatabase()
 
 app.use(express.json())
